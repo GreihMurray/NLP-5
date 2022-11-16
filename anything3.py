93eb7921c3b6e0b491da7fa4cd806800e01be3b2
oalakkad <oalakkad@CAS-MCS-KNQ05N.local>
2022-11-15 20:22:42 -0600

    final try at encoder decoder model

--- anything3.py
+++ anything3.py
@@ -33,8 +33,46 @@ def read_train_file(file_name):
 
     return all_data
 
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ""
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+        print(output_tokens)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if sampled_char == "\n" or len(decoded_sentence) > max_decoder_seq_length:
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.0
+
+        # Update states
+        states_value = [h, c]
+    return decoded_sentence
+
 
 if __name__ == "__main__":
+    batch_size = 1000  # Batch size for training.
+    epochs = 5  # Number of epochs to train for.
+    latent_dim = 256  # Latent dimensionality of the encoding space.
+    num_samples = 45171  # Number of samples to train on.
     input_texts = read_train_file('train-source.txt')
     target_texts = read_train_file('train-target.txt')
     input_characters = set()
@@ -88,3 +126,75 @@ if __name__ == "__main__":
                 decoder_target_data[i, t - 1, target_token_index[char]] = 1.0
         decoder_input_data[i, t + 1 :, target_token_index[" "]] = 1.0
         decoder_target_data[i, t:, target_token_index[" "]] = 1.0
+    
+    # Define an input sequence and process it.
+    encoder_inputs = keras.Input(shape=(None, num_encoder_tokens))
+    encoder = keras.layers.LSTM(latent_dim, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    
+    # We discard `encoder_outputs` and only keep the states.
+    encoder_states = [state_h, state_c]
+    
+    # Set up the decoder, using `encoder_states` as initial state.
+    decoder_inputs = keras.Input(shape=(None, num_decoder_tokens))
+    
+    # We set up our decoder to return full output sequences,
+    # and to return internal states as well. We don't use the
+    # return states in the training model, but we will use them in inference.
+    decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    decoder_dense = keras.layers.Dense(num_decoder_tokens, activation="softmax")
+    decoder_outputs = decoder_dense(decoder_outputs)
+    
+    # Define the model that will turn
+    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+    # model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    
+    # model.compile(
+    # optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"]
+    # )
+    # model.fit(
+    #     [encoder_input_data, decoder_input_data],
+    #     decoder_target_data,
+    #     batch_size=batch_size,
+    #     epochs=epochs,
+    #     validation_split=0.2,
+    # )
+    # Save model
+    # model.save("s2s")
+    
+    model = keras.models.load_model("s2s")
+    
+    encoder_inputs = model.input[0]  # input_1
+    encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output  # lstm_1
+    encoder_states = [state_h_enc, state_c_enc]
+    encoder_model = keras.Model(encoder_inputs, encoder_states)
+    
+    decoder_inputs = model.input[1]  # input_2
+    decoder_state_input_h = keras.Input(shape=(latent_dim,))
+    decoder_state_input_c = keras.Input(shape=(latent_dim,))
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_lstm = model.layers[3]
+    decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+        decoder_inputs, initial_state=decoder_states_inputs
+    )
+    decoder_states = [state_h_dec, state_c_dec]
+    decoder_dense = model.layers[4]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = keras.Model(
+        [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
+    )
+    
+    # Reverse-lookup token index to decode sequences back to
+    # something readable.
+    reverse_input_char_index = dict((i, char) for char, i in input_token_index.items())
+    reverse_target_char_index = dict((i, char) for char, i in target_token_index.items())
+    
+    for seq_index in range(20):
+        # Take one sequence (part of the training set)
+        # for trying out decoding.
+        input_seq = encoder_input_data[seq_index : seq_index + 1]
+        decoded_sentence = decode_sequence(input_seq)
+        print("-")
+        print("Input sentence:", input_texts[seq_index])
+        print("Decoded sentence:", decoded_sentence)
